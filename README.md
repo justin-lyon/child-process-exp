049607129046f9573dcfea4b991110dd99c81cb9
Justin Lyon <jml6487@gmail.com>
2018-11-07 08:39:50 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# child-process-exp
+
+Experimenting with Node's `child_process`.
+
+Read and output a folder and its contents.
---
+++ src/index.js
@@ -0,0 +1,37 @@
+const { spawn } = require('child_process')
+const fs = require('fs')
+
+// Add Options as optional third argument to .spawn()
+// cwd specifies the working directory, defaults to current
+// env specifies the environment variables, defaults to process.env
+// undefined values are ignored
+const options = {
+  cwd: '/Users/justin.lyon/Documents/workspace/bitbucket/lightning-training',
+  env: process.env
+}
+
+var onExit = (code, signal) => {
+  console.log(`Child process exited with code ${code} and signal ${signal}.`)
+}
+
+var onStdOut = data => {
+  console.log(`child stdout:\n${data}`)
+}
+
+var onStdIn = data => {
+  console.log(`child stdout:\n${data}`)
+}
+
+const logFile = fs.createWriteStream('./logs/log.txt')
+const gitStatus = spawn('git', ['status'])
+const gitLog = spawn('git', ['log', '--first-parent', 'origin..HEAD'])
+
+gitLog.stdout.pipe(logFile)
+
+// Required so node process ends. Else, Node may hang on the child process.
+gitLog.on('exit', onExit)
+gitStatus.on('exit', onExit)
+
+// Not necessary for my use case, but event emitters are also possible.
+gitStatus.stdout.on('data', onStdOut)
+gitStatus.stdin.on('data', onStdIn)
